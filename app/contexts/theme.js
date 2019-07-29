a563f77f12be206f66b67e7f95df805f7b0ebd2c
Carlos Loureda <balanzeneto@gmail.com>
2019-07-30 01:10:33 +0200

    refactor: Moved index.js to be a functional component.
    Uses useState, Context.Provider and useMemo

--- app/contexts/theme.js
+++ app/contexts/theme.js
@@ -1,6 +1,4 @@
-import React from 'react'
+import React from "react";
 
-const { Consumer, Provider } = React.createContext()
-
-export const ThemeConsumer = Consumer
-export const ThemeProvider = Provider
+const ThemeContext = React.createContext();
+export default ThemeContext;
--- app/index.js
+++ app/index.js
@@ -1,57 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { ThemeProvider } from './contexts/theme'
-import Loading from './components/Loading'
-import Nav from './components/Nav'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ThemeContext from "./contexts/theme";
+import Loading from "./components/Loading";
+import Nav from "./components/Nav";
 
-const Posts = React.lazy(() => import('./components/Posts'))
-const Post = React.lazy(() => import('./components/Post'))
-const User = React.lazy(() => import('./components/User'))
+const Posts = React.lazy(() => import("./components/Posts"));
+const Post = React.lazy(() => import("./components/Post"));
+const User = React.lazy(() => import("./components/User"));
 
-class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light'
-      }))
-    }
-  }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className='container'>
-              <Nav />
+const App = () => {
+  const [theme, setTheme] = React.useState("light");
+  const toggleTheme = () =>
+    setTheme(theme => (theme === "light" ? "dark" : "light"));
 
-              <React.Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route
-                    exact
-                    path='/'
-                    render={() => <Posts type='top' />}
-                  />
-                  <Route
-                    path='/new'
-                    render={() => <Posts type='new' />}
-                  />
-                  <Route path='/post' component={Post} />
-                  <Route path='/user' component={User} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
+  const value = React.useMemo(
+    () => ({
+      theme,
+      toggleTheme
+    }),
+    [theme]
+  );
+
+  return (
+    <Router>
+      <ThemeContext.Provider value={value}>
+        <div className={theme}>
+          <div className="container">
+            <Nav />
+
+            <React.Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" render={() => <Posts type="top" />} />
+                <Route path="/new" render={() => <Posts type="new" />} />
+                <Route path="/post" component={Post} />
+                <Route path="/user" component={User} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
           </div>
-        </ThemeProvider>
-      </Router>
-    )
-  }
-}
+        </div>
+      </ThemeContext.Provider>
+    </Router>
+  );
+};
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
